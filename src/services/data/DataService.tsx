cc931057a341c682c550da7c1596984d78a6cd74
jmfadeley@gmail.com <jmfadeley@gmail.com>
2020-01-27 12:49:37 -0500

    [MCP-181] Fix to allow environmental variables to be picked up in an Nginx environment, aka the browser...

--- src/services/data/DataService.tsx
+++ src/services/data/DataService.tsx
@@ -2,6 +2,17 @@ import Call from "../../models/Call";
 // import { handleResponse } from "../../models/Call";
 
 /**
+ * Used to allow the client to access global variables,
+ * as "env" is not really available in an Nginx environment.
+ * Move me to a more global location if used again later.
+ */
+declare global {
+  interface Window {
+    REACT_APP_MCP_DATA_MAPPING_UTIL: any;
+  }
+}
+
+/**
  * Data Service, named in the assumption of a unique set of REST calls.
  */
 class DataService {
@@ -32,5 +43,7 @@ class DataService {
 }
 
 export const dataService = new DataService(
-  process.env.REACT_APP_MCP_DATA_MAPPING_UTIL || ""
+  process.env.REACT_APP_MCP_DATA_MAPPING_UTIL ||
+    window.REACT_APP_MCP_DATA_MAPPING_UTIL ||
+    ""
 );
